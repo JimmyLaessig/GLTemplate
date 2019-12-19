c8ba7250fb80c4a84f7edba50918fedcc907118f
Bernhard Rainer <bernhard.rainer@outlook.at>
2019-12-19 22:21:21 +0100

    Removed useless lines from cmake

--- ThirdParty.cmake
+++ ThirdParty.cmake
@@ -9,7 +9,7 @@ set(FETCHCONTENT_BASE_DIR ${THIRDPARTY_BASE_DIR})
 FetchContent_Declare(
   glfw
   GIT_REPOSITORY https://github.com/glfw/glfw.git
-  GIT_TAG        3.3
+  GIT_TAG 3.3
 )
 
 FetchContent_GetProperties(glfw)
@@ -22,7 +22,7 @@ endif()
 set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE )
 set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE )
 set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE )
-set(GLFW_LIBRARY glfw)
+
 
 ##################################################
 # GLEW
@@ -34,15 +34,12 @@ FetchContent_Declare(
   GIT_TAG glew-cmake-2.1.0
 )
 
-
 FetchContent_GetProperties(glew)
 if(NOT glew_POPULATED)
   message(STATUS "Cloning glew: https://github.com/Perlmint/glew-cmake.git")
   FetchContent_Populate(glew)
  message(STATUS "Cloning glew: https://github.com/Perlmint/glew-cmake.git - done")
 endif()
-
-# set(GLEW_LIBRARY ${CMAKE_CURRENT_BINARY_DIR}/lib/glewd.lib)
 
 
 ##################################################
@@ -52,7 +49,7 @@ endif()
 FetchContent_Declare(
   glm
   GIT_REPOSITORY https://github.com/g-truc/glm.git
-  GIT_TAG        0.9.9.5
+  GIT_TAG 0.9.9.5
 )
 
 FetchContent_GetProperties(glm)
